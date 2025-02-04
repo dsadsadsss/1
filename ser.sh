b9187813e9f112f791187d4cf40d1e9a542ce501
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-02-04 19:57:28 +0800

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -3,7 +3,7 @@
 export TMP_ARGO=${TMP_ARGO:-'3x'}
 if [[ "$PWD" == *serv00* ]] || [[ -n "$SSH_CLIENT" ]]; then
 WORKDIR="/home/$(whoami)"
-CRON="cd ${WORKDIR} && pkill -kill -u $(whoami) && export NEZHA_SERVER=$NEZHA_SERVER NEZHA_KEY=$NEZHA_KEY SUB_NAME=$SUB_NAME SUB_URL=$SUB_URL && bash <(curl -Ls https://dl.argo.nyc.mn/ser.sh)"
+CRON="cd ${WORKDIR} && pkill -kill -u $(whoami) && export NEZHA_SERVER=\"$NEZHA_SERVER\" NEZHA_KEY=\"$NEZHA_KEY\" SUB_NAME=\"$SUB_NAME\" SUB_URL=\"$SUB_URL\" && bash <(curl -Ls https://dl.argo.nyc.mn/ser.sh)"
 (crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"tmpapp\"") | crontab -
 yes | crontab -r
 NEW_CRONTAB=""
