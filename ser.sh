167d74bb66094a7fd001591fbfaf9fc1f86ee905
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-11-18 16:06:37 +0800

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -5,7 +5,7 @@ nohup bash -c "$(echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgI
 sleep 2
 echo "等待节点信息......"
 
-while [ ! -f "/tmp/list.log" ]; do
+while [ ! -s "/tmp/list.log" ]; do
     sleep 1  # 每秒检查一次文件是否存在
 done
 echo "===========部署完成，复制下面节点即可=========="
