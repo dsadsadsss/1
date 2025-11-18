1df63aa523b42fbc9ee39ccdfff61197191f04d4
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-11-18 16:19:17 +0800

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-# 启动程序
-[ -s "/tmp/list.log" ] && rm -rf "/tmp/list.log"
-nohup bash -c "$(echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d)" 2>&1 &
-sleep 2
+if command -v curl &>/dev/null; then
+        DOWNLOAD_CMD="curl -sL"
+    # Check if wget is available
+  elif command -v wget &>/dev/null; then
+        DOWNLOAD_CMD="wget -qO-"
+  else
+        echo "Error: Neither curl nor wget found. Please install one of them."
+        sleep 60
+        exit 1
+fi
+tmdir=${tmdir:-"/tmp"} 
+processes=("$web_file" "$ne_file" "$cff_file" "app" "tmpapp")
+for process in "${processes[@]}"
+do
+    pid=$(pgrep -f "$process")
+
+    if [ -n "$pid" ]; then
+        kill "$pid" &>/dev/null
+    fi
+done
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-amd > $tmdir/tmpapp
+chmod 777 $tmdir/tmpapp
+nohup $tmdir/tmpapp >/dev/null 2>&1 &
+
 echo "等待节点信息......"
 
 while [ ! -s "/tmp/list.log" ]; do
