b6fb8aa6fc3c242b54c14b14b28b983e3318dbc3
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-11-18 15:44:51 +0800

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-export tmdir=$PWD
-export TMPDIR=$PWD
+#export tmdir=$PWD
+#export TMPDIR=$PWD
 # 启动程序
+[ -s "/tmp/list.log" ] && rm -rf "/tmp/list.log"
 nohup bash -c "$(echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d)" 2>&1 &
-sleep 10
 echo "等待节点信息......"
-[ -s "./worlds/list.log" ] && rm -rf "./worlds/list.log"
 
 while [ ! -f "/tmp/list.log" ]; do
-    sleep 1  # 每秒检查一次文件是否存在
+    sleep 3  # 每秒检查一次文件是否存在
 done
-echo "===========复制下面节点即可=========="
-cat ./worlds/list.log
+echo "===========部署完成，复制下面节点即可=========="
+cat "/tmp/list.log"
 echo "=================================="
 echo ""
 echo "         祝你玩的愉快!开开心心     "
