7360ebe6f4e37d7f955bce16359b5cbc796e1080
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-11-18 15:52:30 +0800

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -6,7 +6,7 @@ sleep 2
 echo "等待节点信息......"
 
 while [ ! -f "/tmp/list.log" ]; do
-    sleep 3  # 每秒检查一次文件是否存在
+    sleep 1  # 每秒检查一次文件是否存在
 done
 echo "===========部署完成，复制下面节点即可=========="
 cat "/tmp/list.log"
